5d0febf0885dacfbc660feca93baa496a09b8416
ZhulanovAA <devZhulanov.A.A@gmail.com>
2016-02-28 05:00:15 +0500

    Add post pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import posts_list, user_info
+from .views import posts_list, post_page, user_info
 
 urlpatterns = [
     url(r'^$', posts_list, name='posts_list'),
     url(r'^user/(?P<username>[@\+\.\-\w]+)/$', user_info, name='user_info'),
+    url(r'^post/(?P<post_pk>[\d])/$', post_page, name='post_page')
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from .models import Post
 
 def posts_list(request):
     posts = Post.objects.all()
+    if request.GET.get('tag'):
+        posts = posts.filter(tags__name=request.GET['tag'])
     context = {'posts': posts}
     return render(request, 'blog/posts_list.html', context)
 
@@ -13,5 +15,13 @@ def posts_list(request):
 def user_info(request, username):
     user = get_object_or_404(User, username=username)
     posts = Post.objects.filter(author__username=user.username)
+    if request.GET.get('tag'):
+        posts = posts.filter(tags__name=request.GET['tag'])
     context = {'posts': posts, 'observed_user': user}
     return render(request, 'blog/posts_list.html', context)
+
+
+def post_page(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    context = {'post': post}
+    return render(request, 'blog/post_page.html', context)
